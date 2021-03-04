f461c84cfa72a989cc82237aa4dc96b2ec231b99
Javier J Rueda <43324374+javierjrueda@users.noreply.github.com>
2021-03-04 02:37:39 +0100

    Update inicio.js

--- views/inicio.js
+++ views/inicio.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+
 
 const Inicio = ({ navigation }) => {
     return( 
-        <View style={styles.contenedor} >
-        <Text style={styles.title}>Mis aplicaciones:</Text>
-        <Button
-            title="🚀 Criptomonedas"
-            onPress={ () => navigation.navigate('Criptomonedas') }
-        />
-        <Button
-            title="👨🏻‍🎨 Layouts"
-            onPress={ () => navigation.navigate('Layouts') }
-        />
+        <LinearGradient
+                colors={['#432581','#99f0fb']}
+                style={styles.background}
+                start={[0,0]}
+                end={[1,1]}
+        >
+            <View style={styles.contenedor}>
+                <Text style={styles.title}>Mis aplicaciones:</Text>
+                <Button
+                    title="🚀 Criptomonedas"
+                    onPress={ () => navigation.navigate('Criptomonedas') }
+                    color="white"
 
-        <Button
-            title="💅🏼 Administrador de citas"
-            onPress={ () => navigation.navigate('Administrador de Citas') }
-        />
+                />
+                <Button
+                    title="👨🏻‍🎨 Layouts"
+                    onPress={ () => navigation.navigate('Layouts') }
+                    color="white"
+                />
 
-
-        </View>
+                <Button
+                    title="💅🏼 Administrador de citas"
+                    onPress={ () => navigation.navigate('Administrador de Citas') }
+                    color="white"
+                />
+            </View>
+        </LinearGradient>
     );
 }
 
@@ -29,12 +40,24 @@ const styles = StyleSheet.create({
         flex:1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#F2EFE9'
         
     },
     title: {
-        marginVertical: 8
-    }
+        fontFamily: 'FredokaOne-Regular',
+        fontSize: 22, 
+        marginVertical: 20,
+        color: '#fff',
+        marginLeft: 20
+    },
+    background: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        flex: 1
+
+    },
 })
 
 export default Inicio;
